2806159ea1c9c6670eebcd9f220de3da45cd5777
Steve Carter <sweavo@gmail.com>
2016-07-05 12:11:32 +0100

    improved logging and tidied

--- jira_kanban_disable_detail_view.js
+++ jira_kanban_disable_detail_view.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         JIRA Kanban Disable Detail View
+// @name         JIRA Hide Detail No jQuery No checkbox
 // @namespace    https://github.com/sweavo/jiraMonkey
-// @version      1.0
-// @description  Further to https://jira.atlassian.com/browse/GHS-11160 just stop GreenHopper (JIRA agile) opening the detail view at all in Kanban boards
+// @version      0.5
+// @description  Further to https://jira.atlassian.com/browse/GHS-11160 just stop GreenHopper (JIRA agile) opening the detail view at all.
 // @author       sweavo@gmail.com
 // @include      /^https?:\/\/[^/]*\/secure\/RapidBoard.jspa\?rapidView/
 // @grant        none
@@ -10,9 +10,10 @@
 // ==/UserScript==
 // To install: open tamperMonkey dashboard, create a new script, paste this whole file over the whole edit buffer.
 
-// To open the actual issue, it's necessary to shift-click or ctrl-click the issue key.
 
 // TamperMonkey executes this script on DOMContentLoaded
-// We cause the controller to forget how to open the planview.
-GH.WorkController.openDetailsView=function (a){ /* nothing */ };
-
+console.log('JiraHideDetail: TamperMonkey about to neuter openDetailsView.\n');
+GH.WorkController.openDetailsView=function openDetailsView(a) {
+console.log('JiraHideDetail: neutered openDetailsView called on item ' + a + '\n' );
+};
+console.log('JiraHideDetail: TamperMonkey neutered openDetailsView.\n');
