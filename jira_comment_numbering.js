521f3927add4c7dc33d8520bac3bde61e573ddf6
Steve Carter <sweavo@gmail.com>
2019-01-25 17:22:12 +0000

    allow comments to be in reverse order

--- jira_comment_numbering.js
+++ jira_comment_numbering.js
@@ -27,13 +27,13 @@
 
     /* Whether to log stuff to console to show it working */
     const JCN_LOG_TO_CONSOLE=false;
-    
+
     /* Insert child as the first child nodes of container. */
     function prepend( container, child )
     {
         container.insertBefore( child, container.childNodes[0] );
     }
-    
+
     /* In a jira comments block, we start counting at 1 unless the "show more comments"
      * div is visible. In that case, we start at the number of hidden comments + 1
      */
@@ -57,7 +57,7 @@
             return 1 + parseInt( number );
         }
     }
-    
+
     /* true if and only if node is a textnode whose contents match text */
     function isTextNodeMatching( node, text )
     {
@@ -69,7 +69,7 @@
         if ( JCN_LOG_TO_CONSOLE ) console.log( "JCN: new: " + text );
         return ( node.textContent == text ) ;
     }
-    
+
     /* insert numbers to the start of the headers of comments on the browse page */
     function number_comments( )
     {
@@ -79,10 +79,15 @@
             if ( JCN_LOG_TO_CONSOLE ) console.log( "JCN: document was null" );
             return;
         }
-        var descending=false;
-    
+        var issue_activity_sort_links = document.getElementsByClassName('issue-activity-sort-link');
+        if ( JCN_LOG_TO_CONSOLE ) console.log( "JCN: issue_activity_sort_links.length " + issue_activity_sort_links.length );
+
+        var descending = ( issue_activity_sort_links.length )
+                    ? ( 1 == issue_activity_sort_links[0].getElementsByClassName('aui-iconfont-down').length )
+                    : false;
+
         if ( JCN_LOG_TO_CONSOLE ) console.log( "JCN: " + descending );
-    
+
         var comments = document.getElementsByClassName( 'activity-comment' );
         var i;
         var issue_actions_container = document.getElementById( 'issue_actions_container' )
@@ -92,7 +97,7 @@
             return;
         }
         var offset = get_start_point( issue_actions_container );
-    
+
         for ( i = 0; i < comments.length; i++ ) {
             if ( ! comments[i] )
             {
@@ -112,7 +117,7 @@
         }
         if ( JCN_LOG_TO_CONSOLE ) console.log( 'JCN: number_comments done' );
     }
-    
+
     /* insert numbers and set an interval to repeat it. This is because triggering
      * number_comments on the click event of show-more-comments didn't always work.
      */
@@ -121,7 +126,7 @@
         setInterval( number_comments, 2000 );
         number_comments( );
     }
-    
+
     // for non-tampermonkey bootstrapping
     // once the DOM is loaded, start the modifications.
     document.addEventListener( "DOMContentLoaded", number_comments_persistently, false );
