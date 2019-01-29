94f5e10f9660f0f63d2c2a2cfd2f2118caaacadf
Steve Carter <sweavo@gmail.com>
2019-01-29 14:15:08 +0000

    replaced the no-longer working jira hide detail view with one updated for chrome and JIRA 7.1.8

---
+++ jira_hide_detail_view.js
@@ -0,0 +1,27 @@
+==UserScript==
+// @name         JIRA Remove Detail View No jQuery for JIRA Software 7.1.8
+// @namespace    http://tampermonkey.net/
+// @version      7.1.8
+// @description  Close and disable the detail view in backlog
+// @author       sweavo@gmail.com
+// @include      /^https?:\/\/.*\/secure\/RapidBoard\.jspa\?rapidView/
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function remove_detail_view()
+    {
+        console.log('JHDNjQ: remove_detail_view');
+        GH.PlanController.setDetailViewOpenedState(false);
+        GH.PlanView.updateHorizontalPositioning();
+        // And nobble the method
+        GH.PlanController.setDetailViewOpenedState = function( boolValue ) {};
+    }
+
+
+    console.log('JHDNjQ: hello');
+    remove_detail_view();
+})();
+
